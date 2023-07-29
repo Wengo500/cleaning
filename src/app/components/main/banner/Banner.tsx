d68a715d80c6e302e3e5ea31eab44b6e5a8fe723
Wengo500 <wengo500@gmail.com>
2023-07-29 13:49:09 -0400

    finished danner

--- src/app/components/main/banner/Banner.tsx
+++ src/app/components/main/banner/Banner.tsx
@@ -3,19 +3,27 @@
 import style from './banner.module.scss'
 import Image from 'next/image'
 
-import table from '@/images/banner/table.png'
-import Tamara from '@/images/banner/Tamara.png'
-import armchair from '@/images/banner/armchair.png'
-import leaves from '@/images/banner/leaves.png'
+import mainPerson from '@/images/banner/mainPerson.png'
+import girl from '@/images/banner/girl.png'
+import toiletBrush from '@/images/banner/toiletBrush.png'
+import leafRake from '@/images/banner/leafRake.png'
+
+import smog from '@/images/banner/smog.png'
 
 import { useEffect, useState } from 'react'
 
 import Spline from '@splinetool/react-spline';
 
-
+import { Raleway } from '@next/font/google'
+ 
+const font = Raleway({ weight: "500", subsets: ['latin']})
 
 export default function Banner() {
   const [coordinate, setCoordinate] = useState({y:0, x: 0})
+
+  const VW = window.innerWidth
+  const VH = window.innerHeight
+
 
   useEffect(() => {
     document.addEventListener('mousemove', (event) => {
@@ -33,40 +41,50 @@ export default function Banner() {
         <div 
           className={style.mainLayer}
           style={{transform: 
-            `rotateX(${(coordinate.y - window.innerWidth/2) * -.003}deg) 
-            rotateY(${(coordinate.x - window.innerHeight/2) * -.003}deg)`}}
+            `rotateX(${(coordinate.y - window.innerWidth/2) * -.005}deg) 
+            rotateY(${(coordinate.x - window.innerHeight/2) * -.005}deg)`}}
         >
-          <Image
-            className={style.table}
-            src={table}
-            width={200}
-            height={200}
-            alt="Picture of the author"
-          />
+          
           <div className={style.banner__text}>
-              <h1 className={style.title}>Профиссиональная уборка</h1>
-              <p className={style.subtitle}>"Чистота и свежесть дома – наша забота! <span>Полный спектр клининговых услуг.</span> Закажите прямо сейчас!"</p>
-              <button>Start Learning</button>
-              {/* <Spline scene="https://prod.spline.design/oaXlzsYVnD-bR-4C/scene.splinecode" /> */}
+             <h1 className={style.title + ' ' + font.className}>WELCOME <br/> TO <br/>CLEANING HERO</h1>
+             <p className={style.subtitle}>The best cleaning solution</p>
+             <button className={style.batton}>Start Discover</button>
+             {/* <Spline scene="https://prod.spline.design/oaXlzsYVnD-bR-4C/scene.splinecode" /> */}
           </div>
-          <Image
-            className={style.Tamara}
-            src={Tamara}
-            width={350}
-            height={520}
+           
+           <Image
+            className={style.toiletBrush}
+            src={toiletBrush}
+            width={(VH+VW) * 0.15}
+            height={(VH+VW) * 0.35}
             alt="Picture of the author"
           />
           <Image
-            className={style.armchair}
-            src={armchair}
-            width={370}
-            height={370}
+            className={style.leafRake}
+            src={leafRake}
+            width={(VH+VW) * 0.18}
+            height={(VH+VW) * 0.33}
             alt="Picture of the author"
           />
           <Image
-            className={style.leaves}
-            src={leaves}
-            fill
+            className={style.girl}
+            src={girl}
+            width={(VH+VW) * 0.1}
+            height={(VH+VW) * 0.22}
+            alt="Picture of the author"
+          />
+          <Image
+            className={style.mainPerson}
+            src={mainPerson}
+            width={(VH+VW) * 0.32}
+            height={(VH+VW) * 0.27}
+            alt="Picture of the author"
+          />
+          <Image
+            className={style.smog}
+            src={smog}
+            height={400}
+            width={2000}
             alt="Picture of the author"
           />
         </div>
